19e7e3f0608a0e9c08887953cf2c9910b050127b
Andrey Martynov <andremartynov@gmail.com>
2025-10-09 20:01:38 +0300

    1. Added matrix representation

--- 1.graphs/src/main/java/me/lokaur/algorithms/graphs/Main.java
+++ 1.graphs/src/main/java/me/lokaur/algorithms/graphs/Main.java
@@ -3,6 +3,43 @@ package me.lokaur.algorithms.graphs;
 public class Main {
     public static void main(String[] args) {
         nodeBasedRepresentation();
+        matrixBasedRepresentation();
+    }
+
+    private static void matrixBasedRepresentation() {
+        int[][] bandwidthMatrix = {
+                {0, 1500, 2000, 1000, 0, 0}, // A
+                {1500, 0, 0, 0, 0, 1500}, // B
+                {2000, 0, 0, 0, 900, 500}, // C
+                {1000, 0, 0, 0, 2500, 0}, // D
+                {0, 0, 900, 2500, 0, 300}, // E
+                {0, 1500, 500, 0, 300, 0} // F
+                // A, B, C, D, E, F
+        };
+        int[][] packetLossMatrix = {
+                {0, 90, 10, 50, 0, 0}, // A
+                {90, 0, 0, 0, 0, 60}, // B
+                {10, 0, 0, 0, 5, 20}, // C
+                {50, 0, 0, 0, 1, 0}, // D
+                {0, 0, 5, 1, 0, 85}, // E
+                {0, 60, 20, 0, 85, 0} // F
+                // A, B, C, D, E, F
+        };
+
+        System.out.println("Bandwidth matrix:");
+        print(bandwidthMatrix);
+        System.out.println("Packet loss matrix:");
+        print(packetLossMatrix);
+    }
+
+    private static void print(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int col : row) {
+                System.out.printf("%5d", col);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     private static void nodeBasedRepresentation() {
@@ -20,6 +57,8 @@ public class Main {
 
         graph.addEdge("E", "F", 300, 85);
 
+        System.out.println("Graph representation:");
         graph.print();
+        System.out.println();
     }
 }
