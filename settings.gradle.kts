3ae966c2d682e8b699c2442af57c593dbdeea8b2
Andrey Martynov <andremartynov@gmail.com>
2025-12-14 22:22:06 +0300

    8. Added task 8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "algorithms-course"
 
-include("1.graphs", "2.complexity", "3.collection", "4.recursion", "7.hashing")
+include("1.graphs", "2.complexity", "3.collection", "4.recursion", "7.hashing", "8.streams")
 
 dependencyResolutionManagement {
     repositories {
